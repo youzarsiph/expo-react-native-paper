2a07d279988f36c2773adf82e100ec09b6b0a596
Yousef Abu Shanab <josephyousef249@gmail.com>
2024-05-13 23:55:22 -0700

    Update .eslintrc.js

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { ScrollViewStyleReset } from 'expo-router/html'
+import React from 'react'
 
 // This file is web-only and used to configure the root HTML for every
 // web page during static rendering.

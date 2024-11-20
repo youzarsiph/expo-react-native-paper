a2ace47c81a32f18aa66202c0e9c7d6092febfa5
Yousef Abu Shanab <josephyousef249@gmail.com>
2024-11-19 22:31:17 -0800

    add searchbar to stack and drawer header

--- lib/ui/components/DrawerHeader.tsx
+++ lib/ui/components/DrawerHeader.tsx
@@ -1,28 +1,61 @@
 import { DrawerHeaderProps as BaseProps } from '@react-navigation/drawer'
 import { getHeaderTitle } from '@react-navigation/elements'
+import { router } from 'expo-router'
 import React from 'react'
-import { Appbar, AppbarProps } from 'react-native-paper'
+import {
+  Appbar,
+  AppbarProps,
+  IconButton,
+  Searchbar,
+  Tooltip,
+} from 'react-native-paper'
 
 interface DrawerHeaderProps extends AppbarProps {
   navProps: BaseProps
+  withSearchBar?: boolean
 }
 
 const DrawerHeader = (props: DrawerHeaderProps) => (
   <Appbar.Header {...props}>
-    <Appbar.Action
-      icon="menu"
-      onPress={() => props.navProps.navigation.openDrawer()}
-    />
+    {props.withSearchBar ? (
+      <Searchbar
+        value=""
+        icon="menu"
+        onPress={() => router.push('/search')}
+        placeholder="Search Al-Quran"
+        style={{ margin: 8, marginBottom: 16 }}
+        onIconPress={() => props.navProps.navigation.openDrawer()}
+        right={(props) => (
+          <IconButton
+            {...props}
+            icon="cog"
+            onPress={() => router.push('/settings')}
+          />
+        )}
+      />
+    ) : (
+      <>
+        <Tooltip title="Open drawer">
+          <Appbar.Action
+            icon="menu"
+            onPress={() => props.navProps.navigation.openDrawer()}
+          />
+        </Tooltip>
 
-    <Appbar.Content
-      title={getHeaderTitle(props.navProps.options, props.navProps.route.name)}
-    />
+        <Appbar.Content
+          title={getHeaderTitle(
+            props.navProps.options,
+            props.navProps.route.name,
+          )}
+        />
 
-    {props.navProps.options.headerRight
-      ? props.navProps.options.headerRight({
-          canGoBack: props.navProps.navigation.canGoBack(),
-        })
-      : undefined}
+        {props.navProps.options.headerRight
+          ? props.navProps.options.headerRight({
+              canGoBack: props.navProps.navigation.canGoBack(),
+            })
+          : undefined}
+      </>
+    )}
   </Appbar.Header>
 )
 
--- lib/ui/components/StackHeader.tsx
+++ lib/ui/components/StackHeader.tsx
@@ -1,33 +1,54 @@
 import { getHeaderTitle } from '@react-navigation/elements'
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
+import { router } from 'expo-router'
 import React from 'react'
-import { Appbar, AppbarProps } from 'react-native-paper'
+import { Appbar, AppbarProps, Searchbar } from 'react-native-paper'
 
 interface StackHeaderProps extends AppbarProps {
   navProps: NativeStackHeaderProps
+  withSearchbar?: boolean
 }
 
 const StackHeader = (props: StackHeaderProps) => (
   <Appbar.Header {...props}>
-    {props.navProps.options.headerLeft
-      ? props.navProps.options.headerLeft({
-          canGoBack: props.navProps.navigation.canGoBack(),
-        })
-      : undefined}
+    {props.withSearchbar ? (
+      <Searchbar
+        value=""
+        icon="arrow-left"
+        onPress={() => router.push('/search')}
+        placeholder={
+          'Search ' +
+          getHeaderTitle(props.navProps.options, props.navProps.route.name)
+        }
+        style={{ margin: 8, marginBottom: 16 }}
+        onIconPress={() => props.navProps.navigation.goBack()}
+      />
+    ) : (
+      <>
+        {props.navProps.options.headerLeft
+          ? props.navProps.options.headerLeft({
+              canGoBack: props.navProps.navigation.canGoBack(),
+            })
+          : undefined}
 
-    {props.navProps.back ? (
-      <Appbar.BackAction onPress={props.navProps.navigation.goBack} />
-    ) : null}
+        {props.navProps.back ? (
+          <Appbar.BackAction onPress={props.navProps.navigation.goBack} />
+        ) : null}
 
-    <Appbar.Content
-      title={getHeaderTitle(props.navProps.options, props.navProps.route.name)}
-    />
+        <Appbar.Content
+          title={getHeaderTitle(
+            props.navProps.options,
+            props.navProps.route.name,
+          )}
+        />
 
-    {props.navProps.options.headerRight
-      ? props.navProps.options.headerRight({
-          canGoBack: props.navProps.navigation.canGoBack(),
-        })
-      : undefined}
+        {props.navProps.options.headerRight
+          ? props.navProps.options.headerRight({
+              canGoBack: props.navProps.navigation.canGoBack(),
+            })
+          : undefined}
+      </>
+    )}
   </Appbar.Header>
 )
 
